61f25874ac414256c2815ed478afb381746b58a5
chengliang <liangcheng94cl@163.com>
2017-11-20 18:06:33 +0800

    产品详情

--- views/static/bonface-admin/page/products/product.js
+++ views/static/bonface-admin/page/products/product.js
@@ -1,39 +1,90 @@
-layui.config({
-    base: "../../js/"
-  }).use(['layer', 'jquery'], function() {
-      var  $ = layui.jquery;
-      var layer = layui.layer;
-      var langUrl = $('iframe').context.location.search
-      var lang = langUrl.substring(6, langUrl.length)
+$(function() {
+  var langUrl = $('iframe').context.location.search
+  var lang = langUrl.substring(6, langUrl.length)
+  var imgUrl;
+  var data;
+  // 中文情况
+  if(lang === 'cn') {
+    $('.layer').addClass('cn')
+    imgUpLoad('.cn .add', 'cn')
+  }
 
-      $.ajax({
-        type:"get",
-        url:"/api/user/product-list",
-        async:true,
-        data: {
-          lang: lang
-        },
-        success: function (res) {
-          if(res.code == 1) {
-            console.log(res)
-//          $('.cover-img > img').attr('src', res.data.image)
-          }
-          console.log(res)
+  // 英文情况
+  if(lang === 'en') {
+    $('.layer').addClass('en')
+    imgUpLoad('.en .add', 'en')
+  }
+
+  // 韩文情况
+  if(lang === 'kr') {
+    $('.layer').addClass('kr')
+    imgUpLoad('.kr .add', 'kr')
+  }
+
+  $.ajax({
+    type: "get",
+    url: "/api/user/product-list",
+    async: true,
+    data: {
+      lang: lang
+    },
+    success: function(res) {
+      if(res.code == 1) {
+        console.log(res)
+      }
+    }
+  });
+
+  //上传图片
+  function imgUpLoad(select, lang) {
+    $(select).on('click', function() {
+      $(this).next().click()
+    })
+    $('.file').on('change', function(e) {
+      var fileObj = e.target.files[0]
+      common.imgUploadFn(fileObj, 1, '/api/admin/upload-image', function(res) {
+        if(res.code == 1) {
+          imgUrl = res.data.images[0]
         }
-      });
+      })
+    })
+  }
 
-      //点击编辑出现编辑弹窗
-      $('.edit').on('click', function () {
-        $('.layer-wrap').removeClass('none');
-			})
-
-			// 点击关闭弹窗
-			$('.close, .cancel').on('click', function () {
-				$('.layer-wrap').addClass('none');
-			})
-
-			// 点击新增打开弹窗
-			$('#add').on('click', function () {
-        $('.layer-wrap').removeClass('none');
-			})
+  // 点击保存按钮
+  $('.save').on('click', function() {
+    if(!$('#productName').val()) {
+      alert('产品名称不能为空')
+      return
+    }
+    if(!imgUrl) {
+      alert('请选择产品详情')
+      return
+    }
+    data = {
+      name: $('#productName').val(),
+      detail_image: imgUrl,
+      lang: lang
+    }
+    common.ajaxFn('/api/admin/save-product', data, function(res) {
+      console.log(res)
+    })
   })
+
+
+  //点击编辑出现编辑弹窗
+  $('.edit').on('click', function() {
+    $('.layer-wrap').removeClass('none');
+    $('.edit-title').text('编辑产品')
+  })
+
+  // 点击关闭弹窗
+  $('.close, .cancel').on('click', function() {
+    $('.layer-wrap').addClass('none');
+  })
+
+  // 点击新增打开弹窗
+  $('#add').on('click', function() {
+    $('.layer-wrap').removeClass('none');
+    $('.edit-title').text('添加产品')
+  })
+})
